e1bfd8e99e4613afafdc71d0e33f084286f25e4a
wesleyjanse <wesleyjanse@gmail.com>
2021-03-14 19:09:35 +0100

    Fix inputform two way binding

--- src/components/orderentry.component.js
+++ src/components/orderentry.component.js
@@ -1,23 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class OrderEntry extends Component {
-    constructor(props){
-        super(props)
-    }
-
-    componentDidMount(){
-        //TODO: get dish from backend
-    }
-
-    render() {
+const OrderEntry = ({entry, handleChangeDish}) => {
         return(
             <div className="row">
-                <input className="form-control" type="text" value={this.props.value.dishName}/>
-                <input className="form-control" type="number" value={this.props.value.quantity}/>
-                <input className="form-control" step="0.01" type="number" value={this.props.value.price}/>
+                <input onChange={handleChangeDish} className="form-control" type="text" value={entry.dishName}/>
+                <input onChange={handleChangeDish} className="form-control" type="number" value={entry.quantity}/>
+                <input onChange={handleChangeDish} className="form-control" step="0.01" type="number" value={entry.price}/>
             </div>
         )
-    }
-
 }
+
+export default OrderEntry;
